3008d10849f60f58a080c9f8e147e2606b557fd8
Davide Fuoco <2236970@collegemv.qc.ca>
2024-05-06 14:47:28 -0400

    updated tor receive

---
+++ cast.js
@@ -0,0 +1,46 @@
+const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+const context = cast.framework.CastReceiverContext.getInstance();
+const CH = {
+    settings : 'urn:x-cast:settings',
+    game : 'urn:x-cast:game'
+};
+
+context.addCustomMessageListener(CH.settings, function(customEvent) {
+	const pos = customEvent.data.msg;
+    console.log(pos)
+    document.querySelector("#result").innerText = pos;
+    context.sendCustomMessage(CH.settings, undefined, "settings updated")
+});
+
+context.addCustomMessageListener(CH.game, function(customEvent) {
+	const pos = customEvent.data.msg;
+    console.log(pos)
+    document.querySelector("#result").innerText = pos;
+    context.sendCustomMessage(CH.game, undefined, "throwing")
+});
+
+context.addEventListener(cast.framework.system.EventType.READY, () => {
+    if (!castDebugLogger.debugOverlayElement_) {
+        // Enable debug logger and show a 'DEBUG MODE' overlay at top left corner.
+        castDebugLogger.setEnabled(true);
+        // Show debug overlay
+        castDebugLogger.showDebugLogs(true);
+        // Clear log messages on debug overlay
+        castDebugLogger.clearDebugLogs();
+    }
+});
+
+castDebugLogger.loggerLevelByEvents = {
+  'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
+  'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
+}
+
+
+const options = new cast.framework.CastReceiverOptions();
+
+options.customNamespaces = Object.assign({});
+options.customNamespaces[CH.settings] = cast.framework.system.MessageType.JSON;
+options.customNamespaces[CH.game] = cast.framework.system.MessageType.JSON;
+options.disableIdleTimeout = true;
+
+context.start(options);
