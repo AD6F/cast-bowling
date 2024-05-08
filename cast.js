5b93be8da23f4b39624f6f87b0be8128196b623b
Davide Fuoco <2236970@collegemv.qc.ca>
2024-05-08 08:57:50 -0400

    refactor

--- cast.js
+++ cast.js
@@ -1,23 +1,14 @@
-/*
+console.log("VERY START");
 const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
 const context = cast.framework.CastReceiverContext.getInstance();
-const CH = {
-    settings : 'urn:x-cast:setting',
-    game : 'urn:x-cast:game'
-};
+const CH = 'urn:x-cast:setting';
+console.log("INITIALIZE");
 
-context.addCustomMessageListener(CH.settings, (customEvent) => {
+context.addCustomMessageListener(CH, (customEvent) => {
 	const pos = customEvent.data.msg;
     console.log(pos)
     document.querySelector("#result").innerText = pos;
     context.sendCustomMessage(CH.settings, undefined, "settings updated")
-});
-
-context.addCustomMessageListener(CH.game, (customEvent) => {
-	const pos = customEvent.data.msg;
-    console.log(pos)
-    document.querySelector("#result").innerText = pos;
-    context.sendCustomMessage(CH.game, undefined, "throwing")
 });
 
 context.addEventListener(cast.framework.system.EventType.READY, () => {
@@ -32,18 +23,23 @@ context.addEventListener(cast.framework.system.EventType.READY, () => {
     }
 });
 
+console.log("MAKE EVENT LISTENERS");
+
 castDebugLogger.loggerLevelByEvents = {
   'cast.framework.events.category.CORE': cast.framework.LoggerLevel.INFO,
   'cast.framework.events.EventType.MEDIA_STATUS': cast.framework.LoggerLevel.DEBUG
 }
 
+console.log("LOGGERS EVENTS");
+
+console.log("START CAST");
 
 const options = new cast.framework.CastReceiverOptions();
 
 options.customNamespaces = Object.assign({});
-options.customNamespaces[CH.settings] = cast.framework.system.MessageType.JSON;
-options.customNamespaces[CH.game] = cast.framework.system.MessageType.JSON;
+options.customNamespaces[CH] = cast.framework.system.MessageType.JSON;
 options.disableIdleTimeout = true;
 
 context.start(options);
-*/
+
+console.log("LOADING..");
