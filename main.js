3b97191fa7efd2bbeb96133d96ee0be2ae6e6c64
Messicat <112326931+JF195@users.noreply.github.com>
2024-05-20 14:07:48 -0400

    waiting

--- main.js
+++ main.js
@@ -20,9 +20,7 @@ context.addCustomMessageListener(CH.settings,(customEvent) => {
     
     document.querySelector("#result").innerText = pos;
     
-    setTimeout( ()=> {
-        context.sendCustomMessage(CH.game, undefined, {player: settings.players[0]});
-    }, 3000)
+    context.sendCustomMessage(CH.game, undefined, {player: settings.players[0]});
 });
 
 context.addCustomMessageListener(CH.game, (customEvent) => {
